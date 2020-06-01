a9a4a946c6a59d6aafadcc3df929e21d297aafc5
GrishinAnton <asegrishin@sberbank.ru>
2020-06-01 21:18:10 +0300

    update test

--- server/src/migrations/03.migration.ts
+++ server/src/migrations/03.migration.ts
@@ -1,6 +1,6 @@
 export function migration03(connection, fn) {
   connection.query(
-    `ALTER DATABASE dbname CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+    `ALTER DATABASE ${connection.config.database} CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
       ALTER TABLE tags CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
       ALTER TABLE posts CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
     `,
