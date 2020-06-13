3640cfac087d7de25429d9277883f63015d593b0
GrishinAnton <asegrishin@sberbank.ru>
2020-06-13 08:57:25 +0300

    fix

--- client/src/components/FeedFilter/FeedFilter.tsx
+++ client/src/components/FeedFilter/FeedFilter.tsx
@@ -2,11 +2,40 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './feedFilter.module.css';
 
+interface IFilterItems {
+  id: number
+  link: string
+  title: string
+}
+
+const filterItems: IFilterItems[] = [
+  {
+    id: 1,
+    link: 'bestof=1',
+    title: 'Day',
+  },
+  {
+    id: 2,
+    link: 'bestof=7',
+    title: 'Week',
+  },
+  {
+    id: 3,
+    link: 'bestof=30',
+    title: 'Month',
+  },
+];
+
 export const FeedFilter = () => (
   <div className={styles.feedFilter}>
-    <NavLink to="/?bestof=1" activeClassName={styles.feedFilter__link_active} className={styles.feedFilter__link}>Day</NavLink>
-    <NavLink to="/?bestof=7" activeClassName={styles.feedFilter__link_active} className={styles.feedFilter__link}>Week</NavLink>
-    <NavLink to="/?bestof=30" activeClassName={styles.feedFilter__link_active} className={styles.feedFilter__link}>Month</NavLink>
+    <ul>
+      {filterItems && filterItems.map((item) => (
+        <li key={item.id}>
+          <NavLink to={`/?${item.link}`} activeClassName={styles.feedFilter__link_active} className={styles.feedFilter__link}>{item.title}</NavLink>
+        </li>
+      ))}
+
+    </ul>
     <p className={styles.totalPosts}>(0/534)</p>
   </div>
 );
