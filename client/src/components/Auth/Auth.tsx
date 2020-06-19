16e01baca17300ac62208307c91ac7fdc77deada
GrishinAnton <asegrishin@sberbank.ru>
2020-06-19 19:50:13 +0300

    add enum

--- client/src/components/Auth/Auth.tsx
+++ client/src/components/Auth/Auth.tsx
@@ -6,8 +6,13 @@ import SignUp from './SignUp';
 // styles
 import styles from './auth.module.css';
 
+enum AuthType {
+  SIGNIN = 'SIGNIN',
+  SIGNUP = 'SIGNUP',
+}
+
 export const Auth = () => {
-  const [type, setType] = useState<string>('signIn');
+  const [type, setType] = useState<string>(AuthType.SIGNIN);
 
   const submitHandlerSignIn = (e: React.SyntheticEvent) => {
     e.preventDefault();
@@ -21,9 +26,9 @@ export const Auth = () => {
       <div className={styles.authTabs}>
         <button
           className={`${styles.tabsButton} ${
-            type === 'signIn' && styles.tabsButton_active
+            type === AuthType.SIGNIN && styles.tabsButton_active
           }`}
-          onClick={() => setType('signIn')}
+          onClick={() => setType(AuthType.SIGNIN)}
           type="button"
         >
           Войти
@@ -32,14 +37,18 @@ export const Auth = () => {
           className={`${styles.tabsButton} ${
             type === 'signUp' && styles.tabsButton_active
           }`}
-          onClick={() => setType('signUp')}
+          onClick={() => setType(AuthType.SIGNUP)}
           type="button"
         >
           Зарегестрироваться
         </button>
       </div>
-      {type === 'signIn' && <SignIn submitHandler={submitHandlerSignIn} />}
-      {type === 'signUp' && <SignUp submitHandler={submitHandlerSignUp} />}
+      {type === AuthType.SIGNIN && (
+        <SignIn submitHandler={submitHandlerSignIn} />
+      )}
+      {type === AuthType.SIGNUP && (
+        <SignUp submitHandler={submitHandlerSignUp} />
+      )}
     </div>
   );
 };
