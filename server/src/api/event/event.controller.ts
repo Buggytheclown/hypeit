864803d31eda9aea0765b1b65ee22f7025f2d2a7
GrishinAnton <asegrishin@sberbank.ru>
2020-06-26 08:00:48 +0300

    add event controller

---
+++ server/src/api/event/event.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Controller,
+  Get,
+  Req,
+  HttpStatus,
+  HttpException,
+} from '@nestjs/common';
+import { EventModelService } from '../../db/eventModel.service';
+import { BasicEventsPageData } from './event.controller.helpers';
+
+/**  
+TODO
+1. Решить вопрос с camelCase  - event_id
+2. Добавить toggle endpoint для просмотренных
+3. Добавить на возврат всем events isSeen
+4. Формализовать время UTS
+5. Посмотреть что возвращает база, если ничего не найдено
+6. Стоит ли обрабатывать запрос "только для главной" - мне кажется это дело фронта когда запрашивать и где выводить?
+
+TODO NEXT
+1. Добавить swagger
+*/
+@Controller()
+export class AppController {
+  constructor(private readonly eventModelService: EventModelService) {}
+
+  @Get('/api/v1/events')
+  async getPosts(@Req() request): Promise<BasicEventsPageData> {
+    if (request.session.user?.user_id) {
+      const events = await this.eventModelService.getEvents({
+        featureXDays: 14,
+        userId: request.session.user.user_id,
+      });
+      return {
+        events: events || [],
+      };
+    }
+
+    throw new HttpException('Please, authorization', HttpStatus.UNAUTHORIZED);
+  }
+}
